5f6b789be32a21838924e39c9c314c46fd2a1930
Cristobal Jonathan Rios <78310047+crisjona24@users.noreply.github.com>
2022-02-15 09:13:44 -0500

    Update settings.py

--- AdministracionTorneos/settings.py
+++ AdministracionTorneos/settings.py
@@ -73,14 +73,26 @@ WSGI_APPLICATION = 'AdministracionTorneos.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'cristobal24@cristobal24',
+        'HOST': 'cristobal24.postgres.database.azure.com',
+        'SSLMODE' : True,
+        'PORT': 5432,
+    }
+}
 
+#dbname='{your_database}' user='cristobal24@cristobal24' host='cristobal24.postgres.database.azure.com' password='{your_password}' port='5432' sslmode='true'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
